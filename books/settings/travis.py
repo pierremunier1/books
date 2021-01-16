b7c47f5ff2f5b2c0a710abf6154214b519476eba
pierro145 <pierremunier@hotmail.fr>
2021-01-16 16:59:30 +0100

    UPDATE

--- books/settings/travis.py
+++ books/settings/travis.py
@@ -5,8 +5,6 @@ import os
 
 if 'TRAVIS' in os.environ:
 
-    SECRET_KEY = "ma)tgt4@)ffhujtexv87u65)%k5oqtt$&yn99e=p$_&fl!ryti"
-
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
