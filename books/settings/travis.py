80212a05f883733097c8e36eaee6be16bfce836c
pierro145 <pierremunier@hotmail.fr>
2021-01-16 16:48:42 +0100

    update

--- books/settings/travis.py
+++ books/settings/travis.py
@@ -1,17 +1,19 @@
 from . import *
-import environ
+
+import os 
 
 
-env = environ.Env()
-environ.Env.read_env()
+if 'TRAVIS' in os.environ:
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
-}
+    SECRET_KEY = "ma)tgt4@)ffhujtexv87u65)%k5oqtt$&yn99e=p$_&fl!ryti"
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        },
+    }
